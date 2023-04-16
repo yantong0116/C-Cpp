2d263e081bd706830ff6d55b5c536085460baa8a
DESKTOP-0DG591Q\mayta <mayta51654944@gmail.com>
2023-04-16 18:17:48 +0800

    A simple C program printing a line of text

---
+++ Study/CH2 Introduction to C Programming/2_2_A_Simple_C_Program_Printing_a_Line_of_Text.c
@@ -0,0 +1,10 @@
+// Fig. 2.1: fig02_01.c
+// A first program in C.
+
+#include <stdio.h>
+
+// function main begins program execution
+int main(void)
+{
+    printf("Welcome to C!\n");
+} // end function main
