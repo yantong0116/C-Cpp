a066808a4a409da42975fd54e7f10fb7c4c59cb9
Yan <51469882+yantong0116@users.noreply.github.com>
2023-05-11 14:23:31 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,9 +4,12 @@ import requests
 html = requests.get('https://github.com/yantong0116/C-Cpp-Learning').content
 
 soup = BeautifulSoup(html, 'html.parser')
+print(soup.encode("utf-8"))
+
 # quote = float(soup.find(id='clipboard-copy-content-win-create-the-runner-and-start-the-configuration-experience').get_text())
 # quote = soup.find(id='js-global-screen-reader-notice').get_text()
 
 # print(quote)
 
-print(soup)
+# print(soup)
+
