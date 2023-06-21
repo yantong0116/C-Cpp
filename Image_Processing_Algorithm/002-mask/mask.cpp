fda10d2b958648a88e4386bca186d0b06a0039a5
yantong0116 <mayta51654944@gmail.com>
2023-06-21 13:08:32 +0800

    Mask

---
+++ Image_Processing_Algorithm/002-mask/mask.cpp
@@ -0,0 +1,44 @@
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+	Mat original = imread("dice.jpg", IMREAD_GRAYSCALE);
+	Mat mask = imread("dice.jpg", IMREAD_GRAYSCALE);
+	Mat result = imread("dice.jpg", IMREAD_GRAYSCALE);
+	Mat pattern = imread("pattern.jpg", IMREAD_GRAYSCALE);
+
+	int thresh_value = 200;
+
+	//create mask: binary filter
+	for (int i = 0; i < mask.rows; i++) {
+		for (int j = 0; j < mask.cols; j++) {
+			if (mask.at<uchar>(i, j) > thresh_value) {
+				mask.at<uchar>(i, j) = (uchar)(255);
+			}
+			else {
+				mask.at<uchar>(i, j) = (uchar)(0);
+			}
+		}
+	}
+
+	for (int i = 0; i < result.rows; i++) {
+		for (int j = 0; j < result.cols; j++) {
+			if (mask.at<uchar>(i, j) == 255) {
+				result.at<uchar>(i, j) = pattern.at<uchar>(i, j);
+			}
+		}
+	}
+	namedWindow("Mask", WINDOW_AUTOSIZE);
+	imshow("Mask", mask);
+	namedWindow("Result", WINDOW_AUTOSIZE);
+	imshow("Result", result);
+	namedWindow("Pattern", WINDOW_AUTOSIZE);
+	imshow("Pattern", pattern);
+	waitKey(0);
+}
